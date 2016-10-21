ae34c92b0ed22c152e401bb608de7c79381e2724
tg <tgdevjs@gmail.com>
2016-10-20 22:48:58 -0400

    Initial commit ios only.

---
+++ readme.md
@@ -0,0 +1,5 @@
+###react-native-server-monitor
+react native server monitor app
+
+udemy:
+https://www.udemy.com/react-native-projects
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return <Text>App</Text>
+}
+
+export default App;
