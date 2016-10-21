2d1b647533e57d233a11e5843f8a4a3d5e2e0428
tg <tgdevjs@gmail.com>
2016-10-21 08:52:15 -0400

    Updated App component.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-const App = () => {
-  return <Text>App</Text>
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container} >
+        <Text>Server Monitor</Text>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
